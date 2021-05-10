3ad22b2576bb71522ae13d96c371668fe07c900b
ajnart <thomas.camlong@epitech.eu>
2021-05-10 11:00:37 +0200

    ✨ Default C and CPP testing files.

---
+++ cli/tests/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** MyTeams
+** File description:
+** test
+*/
+
+#include <gtest/gtest.h>
+
+TEST(Main, dummy)
+{
+    ASSERT_EQ(0, 0);
+}
---
+++ server/tests/main.cpp
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2021
+** MyTeams
+** File description:
+** test
+*/
+
+#include <gtest/gtest.h>
+
+TEST {Main, Nothing} { ASSERT(0, 0); }
