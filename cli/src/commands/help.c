ec47f5d543dfca583ddfd4811ad3ead287d54ee0
ajnart <thomas.camlong@epitech.eu>
2021-05-19 02:36:16 +0200

    ✨ Modify help command in CLI

--- cli/src/commands/help.c
+++ cli/src/commands/help.c
@@ -9,7 +9,8 @@
 #include <unistd.h>
 #include "commands.h"
 
-int help(connection_t __attribute__((unused)) *connection)
+int help(connection_t __attribute__((unused)) * connection,
+    char __attribute__((unused)) * str)
 {
     printf(
         "•/help:\t show help\n"
@@ -17,14 +18,18 @@ int help(connection_t __attribute__((unused)) *connection)
         "•/logout:\t disconnect the client from the server\n"
         "•/users:\t get the list of all users that exist on the domain\n"
         "•/user\t [\"user_uuid\"] : get information about a user\n"
-        "•/send\t [\"user_uuid\"] [\"message_body\"] : send a message to a user\n"
-        "•/messages\t [\"user_uuid\"] : list all messages exchange with an user\n"
+        "•/send\t [\"user_uuid\"] [\"message_body\"] : send a message to a "
+        "user\n"
+        "•/messages\t [\"user_uuid\"] : list all messages exchange with an "
+        "user\n"
         "•/subscribe\t [\"team_uuid\"] : subscribe to the event of a team and "
         "its sub directories (enable reception of all events from a team)\n"
-        "•/subscribed\t ?[\"team_uuid\"] : list all subscribed teams or list all "
+        "•/subscribed\t ?[\"team_uuid\"] : list all subscribed teams or list "
+        "all "
         "users subscribed to a team\n"
         "•/unsubscribe\t [\"team_uuid\"] : unsubscribe from a team\n"
-        "•/use\t ?[\"team_uuid\"] ?[\"channel_uuid\"] ?[\"thread_uuid\"] : use "
+        "•/use\t ?[\"team_uuid\"] ?[\"channel_uuid\"] ?[\"thread_uuid\"] : "
+        "use "
         "specify a context team/channel/thread\n"
         "•/create:\t based on what is being used create the sub resource (see "
         "documentation)\n"
