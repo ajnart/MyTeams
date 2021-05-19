8ba5da4959b8623a7e47712f5a08784bfd069940
ajnart <thomas.camlong@epitech.eu>
2021-05-19 02:36:03 +0200

    ✨ Add login command

--- cli/src/commands/log.c
+++ cli/src/commands/log.c
@@ -5,14 +5,26 @@
 ** login
 */
 
+#include <string.h>
 #include "definitions.h"
+#include "logging_client.h"
 
-int login(connection_t *command)
+// Strtok_r is used two times to catch the username used to log-in.
+int login(connection_t *connection, char *str)
 {
+    char *rest = str;
+    const char *token = NULL;
+    if (str == NULL || str[0] != '/')
+        return (ERROR);
+    token = strtok_r(rest, " \n", &rest);
+    token = strtok_r(rest, " \n", &rest);
+    // Send command line gotten by the CLI to the server
+    // example: /login thomas -> /login thomas
+    send(connection->socket_fd, str, strlen(str), 0);
     return (OK);
 }
 
-int logout(connection_t *command)
+int logout(connection_t *connection, char *str)
 {
     return (OK);
 }
