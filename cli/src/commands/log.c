ee32281c9c785b1a491cf60c22f74bf0156496de
ajnart <thomas.camlong@epitech.eu>
2021-05-20 19:28:01 +0200

    🔒️ Improve login functions

--- cli/src/commands/log.c
+++ cli/src/commands/log.c
@@ -10,25 +10,27 @@
 #include <string.h>
 #include <unistd.h>
 #include <uuid/uuid.h>
-/* For puts()/printf() */
-#include <stdio.h>
-/* For uuid_generate() and uuid_unparse() */
-#include <uuid/uuid.h>
 #include "definitions.h"
 #include "logging_client.h"
 
 // Strtok_r is used two times to catch the username used to log-in.
 int login(connection_t *connection, char *str)
 {
-    char *answer = NULL;
+    char answer[BLOCK];
+    char *rest = strdup(str);
+    const char *token = NULL;
+
     if (str == NULL || str[0] != '/')
         return (ERROR);
-    // Send command line gotten by the CLI to the server
-    // example: /login thomas -> /login thomas
+    token = strtok_r(rest, " \n", &rest);
+    token = strtok_r(rest, " \n", &rest);
+    if (token == NULL) {
+        printf("[E] You need a name to login!\n");
+        return (ERROR);
+    }
     send(connection->socket_fd, str, strlen(str), 0);
     if (read(connection->socket_fd, answer, BLOCK) <= 0)
         return (ERROR);
-    // Got back an UUID from the server, applying it to the uuid field in the connection structure.
     if (uuid_parse(answer, connection->uuid) != 0)
         return (ERROR);
     return (OK);
@@ -36,9 +38,8 @@ int login(connection_t *connection, char *str)
 
 int logout(connection_t *connection, char *str)
 {
-    char *answer = NULL;
-    if (connection->connected == false)
-    {
+    char answer[BLOCK];
+    if (connection->connected == false) {
         printf("You are not currently logged-in.\n");
         return (ERROR);
     }
