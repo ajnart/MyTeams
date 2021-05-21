2e655ba9f23852b7588538b5d078c083afa3ee51
ajnart <thomas.camlong@epitech.eu>
2021-05-21 16:53:00 +0200

    🎨 Reformat server structure

--- server/include/server.h
+++ server/include/server.h
@@ -8,12 +8,15 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
+#include <sys/socket.h>
 #include "client.h"
 #include "teams.h"
 
 typedef struct server {
     clients *clients;
     team *teams;
-} __attribute__((aligned(16))) server;
+    fd_set fd;
+    int server_socket;
+} __attribute__((aligned(BITS))) server;
 
 #endif /* !SERVER_H_ */
